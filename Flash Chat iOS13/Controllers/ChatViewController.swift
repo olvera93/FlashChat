cb2811b7a1c8a1834ec9d571b9ccd1e6f7120546
Gonzalo Olvera Monroy <42697554+olvera93@users.noreply.github.com>
2022-01-19 16:58:57 -0600

    [UPDATE] App
    El usuario se puede registrar, iniciar sesión y cerrar sesión en la aplicación

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -2,11 +2,12 @@
 //  ChatViewController.swift
 //  Flash Chat iOS13
 //
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
+//  Created by Gonzalo Olvera Monroy on 18/01/2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
 //
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
 
@@ -16,10 +17,26 @@ class ChatViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = "⚡️FlashChat"
+        navigationItem.hidesBackButton = true
+        
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
     }
     
-
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        
+        let firebaseAuth = Auth.auth()
+        
+        do {
+            try firebaseAuth.signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
+        
+        
+    }
+    
 }
