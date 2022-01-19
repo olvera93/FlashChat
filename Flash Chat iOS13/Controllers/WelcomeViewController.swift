e061f4a3a4ae24cd4d5325d5bca82f5f2ea6574a
Gonzalo Olvera Monroy <42697554+olvera93@users.noreply.github.com>
2022-01-19 16:09:36 -0600

    [ADD] Firebase
    Se agregó firebase al proyecto

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,19 +7,20 @@
 //
 
 import UIKit
+import CLTypingLabel
+
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
         titleLabel.text = ""
         var charIndex = 0.0
         let titleText = "⚡️FlashChat"
-    
+        
         for letter in titleText {
             
             Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
@@ -29,8 +30,8 @@ class WelcomeViewController: UIViewController {
             charIndex += 1
             
         }
-       
+        
     }
     
-
+    
 }
